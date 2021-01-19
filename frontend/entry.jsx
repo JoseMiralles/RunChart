3cfda54d1095ea59be0cae1116e5a67fc6e3d8ea
Jose Miralles <josemiralles1991@gmail.com>
2021-01-19 17:28:21 -0500

    React now rendering

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,1 +1,11 @@
-console.log("hi!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    ReactDOM.render(
+        <h1>Hello from entry.js</h1>,
+        document.getElementById("root")
+    );
+
+});

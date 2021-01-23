f620c10e54b2751ad5cb15e55588a89824eae16f
Jose Miralles <josemiralles1991@gmail.com>
2021-01-23 16:55:50 -0500

    Polyline can now be drawn on map.

--- frontend/components/route_builder/route_builder.jsx
+++ frontend/components/route_builder/route_builder.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
-import loadGMaps from "../../scripts/load_g_maps";
+import { loadGMaps, googleMapStyles } from "../../scripts/googleMapsUtils";
 
 export default class RouteBuilder extends React.Component {
 
     constructor(props){
         super(props);
+
         this.state = {
             GMapsLoaded: false
         }
+
+        this.handleMapClick = this.handleMapClick.bind(this);
     }
 
     render(){
@@ -32,16 +35,44 @@ export default class RouteBuilder extends React.Component {
     }
     
 
-
     setUpMap(){
+        // Setup map with options.
         const mapOptions = {
             center: {
-                lat: 37.773972,
-                lng: -122.431297
+                lat: 40.78151480597849,
+                lng: -73.9667305095721
             }, // San Francisco coords
-            zoom: 13
+            zoom: 14,
+            disableDefaultUI: true,
+            zoomControl: true,
+            styles: googleMapStyles
         };
         this.map = new google.maps.Map(this.refs.map, mapOptions);
+
+        // Initialize the polyline, and add it to the map.
+        this.poly = new google.maps.Polyline({
+            strokeColor: "#F15025",
+            strokeOpacity: 1.0,
+            strokeWeight: 7,
+        });
+        this.poly.setMap(this.map);
+
+        // Add click event listener for the map.
+        this.map.addListener("click", this.handleMapClick);
+    }
+
+    handleMapClick(e){
+        const path = this.poly.getPath();
+        path.push(e.latLng);
+
+        // Add a marker if this is the first point.
+        if (path.length === 1) {
+            new google.maps.Marker({
+              position: e.latLng,
+              title: "#" + path.getLength(),
+              map: this.map,
+            });
+        }
     }
 
 }
---
+++ frontend/scripts/googleMapsUtils.js
@@ -0,0 +1,176 @@
+import { googleMapsKey } from "../keys";
+
+export const loadGMaps = (callback) => {
+  const existingScript = document.getElementById('googleMaps');
+  if (!existingScript) {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsKey}`;
+    script.id = 'googleMaps';
+    document.body.appendChild(script);
+    script.onload = () => {
+      if (callback) callback();
+    };
+  }
+  if (existingScript && callback) callback();
+};
+
+export const googleMapStyles = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
